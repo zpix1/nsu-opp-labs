4934c3d7a4c9bcc80dcf58fd3f90095b759f8ce2
Ivan Baksheev <zpix-dev@list.ru>
2021-02-11 18:39:48 +0700

    lab1: memory opt + c style rewrite

--- 1/main.cpp
+++ 1/main.cpp
@@ -3,114 +3,86 @@
 #include <cassert>
 #include <math.h>
 
-using namespace std;
 
 const double EPS = 1e-5;
-using VD = vector<double>;
 
-struct Matrix {
-    int N;
-    double* data;
-    Matrix(int _N) {
-        N = _N;
-        data = (double*)malloc(N * N * sizeof(double));
-    }
-    ~Matrix() {
-        free(data);
-    }
-    void init(double v) {
-        for (int i = 0; i < N*N; i++) {
-            data[i] = v;
+void mulMV(const double* A, const double* x, int N, double* res)  {
+    for (int i = 0; i < N; i++) {
+        res[i] = 0;
+        for (int j = 0; j < N; j++) {
+            res[i] += A[i * N + j] * x[j];
         }
     }
-    double get(int i, int j) const {
-        return data[i * N + j];
-    }
-    void set(int i, int j, double v) {
-        data[i * N + j] = v;
-    }
-    VD operator*(const VD& v) const {
-        assert(N == v.size());
-        VD res(N, 0.0);
+}
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                res[i] += data[i * N + j] * v[j];
-            }
-        }
-        return res;
-    }
-};
-
-double operator*(const VD& a, const VD& b) {
-    assert(a.size() == b.size());
+double scalar(const double* a, const double* b, int N) {
     double res = 0;
-    for (int i = 0; i < a.size(); i++) {
+    for (int i = 0; i < N; i++) {
         res += a[i] * b[i];
     }
     return res;
 }
 
-VD operator*(double k, const VD& b) {
-    VD res(b.size());
-    for (int i = 0; i < b.size(); i++) {
-        res[i] = k * b[i];
+void fill(double* x, int N, double value) {
+    for (int i = 0; i < N; i++) {
+        x[i] = value;
     }
-    return res;
 }
 
-VD operator+(double k, const VD& b) {
-    VD res(b.size());
-    for (int i = 0; i < b.size(); i++) {
-        res[i] = k + b[i];
-    }
-    return res;
-}
+void solve(const double* A, const double* b, int N, double* x) {
+    std::cout << "STARTED SOLVING " << std::endl;
+    double* r = new double[N];
+    double* z = new double[N];
+    double* Az = new double[N];
+    double* buf1 = new double[N];
 
-VD operator-(const VD& a, const VD& b) {
-    assert(a.size() == b.size());
-    VD res(a.size());
-    for (int i = 0; i < a.size(); i++) {
-        res[i] = a[i] - b[i];
+    fill(x, N, 0.0);
+    mulMV(A, x, N, buf1);
+    for (int i = 0; i < N; i++) {
+        r[i] = b[i] - buf1[i];
+        z[i] = r[i];
     }
-    return res;
-}
+    double r_square = scalar(r, r, N);
+    double b_square = scalar(b, b, N);
 
-VD operator+(const VD& a, const VD& b) {
-    assert(a.size() == b.size());
-    VD res(a.size());
-    for (int i = 0; i < a.size(); i++) {
-        res[i] = a[i] + b[i];
-    }
-    return res;
-}
-
-VD solve(const Matrix& A, const VD& b) {
-    cout << "STARTED SOLVING " << endl;
-    VD x(A.N, 0.0);
-    VD r = b - A*x;
-    VD z = r;
-    double r_square = r * r;
-    double b_square = b * b;
     int iter;
     for (iter = 0; iter < 100; iter++) {
-        cout << "ITER: " << iter << endl;
-        VD Az = A*z;
-        double alpha = r_square / (Az * z);
-        x = x + alpha * z;
+        std::cout << "ITER: " << iter << std::endl;
 
-        VD new_r = r - alpha * Az;
-        double r_new_square = new_r * new_r;
+        mulMV(A, z, N, Az);
+
+        double alpha = r_square / scalar(Az, z, N);
+        for (int i = 0; i < N; i++) {
+            x[i] = x[i] + alpha * z[i];
+        }
+
+        for (int i = 0; i < N; i++) {
+            buf1[i] = r[i] - alpha * Az[i];
+        }
+
+        double r_new_square = scalar(buf1, buf1, N);
         double beta = r_new_square / r_square;
-        r = new_r;
-        r_square = r_new_square;
-        z = r + beta * z;
 
-        if ((r_square / b_square) < EPS)
+        for (int i = 0; i < N; i++) {
+            r[i] = buf1[i];
+        }
+
+        r_square = r_new_square;
+
+        for (int i = 0; i < N; i++) {
+            z[i] = r[i] + beta * z[i];
+        }
+
+        if ((r_square / b_square) < EPS) {
             break;
+        }
     }
-    cout << "DONE" << endl;
-    return x;
+
+    std::cout << "DONE" << std::endl;
+    delete[] r;
+    delete[] z;
+    delete[] Az;
+    delete[] buf1;
 }
 
 int main() {
@@ -125,28 +97,36 @@ int main() {
     // VD res = solve(A, b);
 
     // === 2 ===
-    Matrix A(N);
-    A.init(1.0);
+    double* A = new double[N*N];
+    double* u = new double[N];
+    double* b = new double[N];
+    double* res = new double[N];
+    
+    fill(A, N*N, 1.0);
     for (int i = 0; i < N; i++) {
-        A.set(i, i, 2.0);
+        A[i * N + i] = 2.0;
     }
-    VD u(N);
     for (int i = 0; i < N; i++) {
         u[i] = sin(2*M_PI*i / N) * 100;
     }
-    VD b = A*u;
-    VD res = solve(A, b);
-    cout << "VECTOR u: " << endl;
+    mulMV(A, u, N, b);
+    solve(A, b, N, res);
+    std::cout << "VECTOR u: " << std::endl;
     for (int i = 0; i < N; i++) {
-        cout << u[i] << " ";
+        std::cout << u[i] << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
 
-    cout << "RESULT" << endl;
+    std::cout << "RESULT" << std::endl;
     for (int i = 0; i < N; i++) {
-        cout << res[i] << " ";
+        std::cout << res[i] << " ";
     }
-    cout << endl;
+    std::cout << std::endl;
+
+    delete[] A;
+    delete[] u;
+    delete[] b;
+    delete[] res;
 
     return 0;
 }
