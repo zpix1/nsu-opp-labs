04a77cc5c267d854aff1ba574f70fa15c0ea23c9
Ivan Baksheev <zpix-dev@list.ru>
2021-03-07 20:50:36 +0700

    lab2: scripts update

--- 2/versions/analyze_files.py
+++ 2/versions/analyze_files.py
@@ -6,31 +6,34 @@ import matplotlib.pyplot as plt
 
 def get_time(filename):
     data = open(filename).read()
-    print(filename)
-    res = re.findall(r'DONE: (\d+.\d+)', data)[0]
+    try:
+        res = re.findall(r'DONE: (\d+.\d+)', data)[0]
+    except:
+        print('Not found {}'.format(filename))
+        exit(1)
     return float(res)
 
 first_times = [
     None,
-    get_time(glob.glob('run_mul_version_1_1_1-1.sh.o*')[0]),
-    get_time(glob.glob('run_mul_version_2_1_1-1.sh.o*')[0])
+    get_time(glob.glob('rmv_1_1_1-1.sh.o*')[0]),
+    get_time(glob.glob('rmv_2_1_1-1.sh.o*')[0])
 ]
 
 total = {}
 
-all_files = glob.glob('run_mul*.sh')
+all_files = glob.glob('rmv_*.sh')
 
 for runner in all_files:
-    for result in glob.glob('run_mul*.o*'):
+    for result in glob.glob('rmv_*.o*'):
         if result.startswith(runner):
             break
     else:
         pass
         print('NOT FOUND:', runner)
 
-for f in glob.glob('run_mul*.o*'):
+for f in glob.glob('rmv_*.o*'):
     # print(f)
-    res = re.match(r'run_mul_version_(\d+)_(\d+)_\d+-\d+.sh.o(\d+)', f)
+    res = re.match(r'rmv_(\d+)_(\d+)_\d+-\d+.sh.o(\d+)', f)
     version = int(res.group(1))
     ncp = int(res.group(2))
     result = get_time(f)
@@ -43,6 +46,7 @@ for f in glob.glob('run_mul*.o*'):
 
 ans = collections.defaultdict(list)
 
+print(f'version\tC\tT\tA\tE')
 for v in sorted(total.keys()):
     version = v[0]
     ncp = v[1]
@@ -52,31 +56,31 @@ for v in sorted(total.keys()):
     ans['{}_{}'.format(version, 'Sp')].append(total[v][1])
     ans['{}_{}'.format(version, 'Ep')].append(total[v][2])
 
-data = dict(ans)
+if False:
+    data = dict(ans)
 
-def gen_plot(x, y, plot, name, v):
-    if name == 'T':
-        plot.set_title('Время выполнения, с.' + ' (в. {})'.format(v))
-        plot.set_ylim([0, 70])
-    if name == 'Sp':
-        plot.set_title('Ускорение' + ' (в. {})'.format(v))
-        plot.set_ylim([0, 10])
-    if name == 'Ep':
-        plot.set_title('Эффективность, %' + ' (в. {})'.format(v))
-        plot.set_ylim([0, 100])
-    plot.plot(x, y)
-    plot.set_xticks(x)
-    return plot
+    def gen_plot(x, y, plot, name, v):
+        if name == 'T':
+            plot.set_title('Время выполнения, с.' + ' (в. {})'.format(v))
+            plot.set_ylim([0, 70])
+        if name == 'Sp':
+            plot.set_title('Ускорение' + ' (в. {})'.format(v))
+            plot.set_ylim([0, 10])
+        if name == 'Ep':
+            plot.set_title('Эффективность, %' + ' (в. {})'.format(v))
+            plot.set_ylim([0, 100])
+        plot.plot(x, y)
+        plot.set_xticks(x)
+        return plot
 
-fig, plots = plt.subplots(nrows=2, ncols=3)
+    fig, plots = plt.subplots(nrows=2, ncols=3)
 
-i = 0
-for v in ['1', '2']:
-    for t in ['T', 'Sp', 'Ep']:
-
-        x = data['{}_{}'.format(v, 'PC')]
-        y = data['{}_{}'.format(v, t)]
-        gen_plot(x, y, plots.flatten()[i % 3], t, v)
-        i += 1
-fig.tight_layout()
-fig.savefig('results_plots.png', dpi=900)
+    i = 0
+    for v in ['1', '2']:
+        for t in ['T', 'Sp', 'Ep']:
+            x = data['{}_{}'.format(v, 'PC')]
+            y = data['{}_{}'.format(v, t)]
+            gen_plot(x, y, plots.flatten()[i % 3], t, v)
+            i += 1
+    fig.tight_layout()
+    fig.savefig('results_plots.png', dpi=900)
--- 2/versions/generate_versions.py
+++ 2/versions/generate_versions.py
@@ -16,13 +16,29 @@ g++ -Wall -std=c++11 -fopenmp main{VERSION}.cpp -o main{VERSION}{NODES}{CPUS}{NP
 OMP_NUM_THREADS={NPC} ./main{VERSION}{NODES}{CPUS}{NPC}
 '''
 
-types = [1, 2]
-cores = [1, 2, 4, 8, 12, 16, 24]
+oldbase = '''
+#!/bin/bash
+
+# NPC=mpiprocs=select*ncpus
+# 1 <= select <= 2
+
+#PBS -l select={NODES}:ncpus={CPUS}:mpiprocs={NPC}:mem=4000m,place=free
+#PBS -l walltime=01:01:00
+
+cd $PBS_O_WORKDIR
+echo "I run on node: `uname -n`"
+echo "My working directory is: $PBS_O_WORKDIR"
+
+MPI_PATH=/opt/intel/impi/5.0.1.035/intel64/bin
+
+$MPI_PATH/mpicxx main{VERSION}.cpp -o main{VERSION}{NODES}{CPUS}{NPC} -std=c++11
+$MPI_PATH/mpirun -machinefile $PBS_NODEFILE -np {NPC} ./main{VERSION}{NODES}{CPUS}{NPC}
+'''
 
 def write_version(nodes, cpus, version):
     npc = nodes * cpus
     script = base.format(NODES=nodes, CPUS=min(cpus, 24), NPC=npc, VERSION=version)
-    fname = f'run_mul_version_{version}_{npc}_{nodes}-{cpus}.sh'
+    fname = f'rmv_{version}_{npc}_{nodes}-{cpus}.sh'
     with open(fname, 'w') as f:
         f.write(script)
     os.system(f'chmod +x {fname}')
@@ -32,20 +48,20 @@ write_version(1, 1, 1)
 write_version(1, 2, 1)
 write_version(1, 4, 1)
 write_version(1, 8, 1)
-write_version(1, 12, 1)
-write_version(1, 16, 1)
-write_version(1, 20, 1)
-write_version(1, 24, 1)
-write_version(1, 28, 1)
-write_version(1, 32, 1)
+write_version(2, 6, 1)
+write_version(2, 8, 1)
+write_version(2, 10, 1)
+write_version(2, 12, 1)
+# write_version(4, 7, 1)
+# write_version(4, 8, 1)
 
 write_version(1, 1, 2)
 write_version(1, 2, 2)
 write_version(1, 4, 2)
 write_version(1, 8, 2)
-write_version(1, 12, 2)
-write_version(1, 16, 2)
-write_version(1, 20, 2)
-write_version(1, 24, 2)
-write_version(1, 28, 2)
-write_version(1, 32, 2)
+write_version(2, 6, 2)
+write_version(2, 8, 2)
+write_version(2, 10, 2)
+write_version(2, 12, 2)
+# write_version(4, 7, 2)
+# write_version(4, 8, 2)
